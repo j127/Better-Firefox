b865df70b3f996fb37ba76b3cc656ef3f6da21e8
J. Cohen <hostelmarketing@gmail.com>
2015-08-17 09:26:24 -0700

    Update comments

--- user.js
+++ user.js
@@ -2,19 +2,19 @@
 // User Interface
 // ======================
 
-// Don't autofill URL bar
+// Don't autofill URL bar. It's slower due to unpredictability.
 user_pref("browser.urlbar.autoFill", false);
 
-// Don't trim URLs
+// Don't trim URLs. It masks the true address.
 user_pref('browser.urlbar.trimURLs', false);
 
-// Open new tabs in background
+// Open new tabs in background for faster browsing.
 user_pref("browser.search.context.loadInBackground", true);
 
-// Disable new tab animation
+// Disable new tab animation, because animation is annoying.
 user_pref('browser.tabs.animate', false);
 
-// Don't select entire URL
+// Don't select entire URL. Ctrl-L already does that.
 user_pref("browser.urlbar.clickSelectsAll", false);
 
 // ======================
@@ -25,7 +25,6 @@ user_pref("browser.urlbar.clickSelectsAll", false);
 // See http://news.slashdot.org/story/15/08/14/2321202/how-to-quash-firefoxs-silent-requests
 // and: http://www.ghacks.net/2015/08/16/block-firefox-from-connecting-to-sites-when-you-hover-over-links/
 user_pref("network.http.speculative-parallel-limit", 0);
-
 
 // ======================
 // Privacy
@@ -39,6 +38,7 @@ user_pref('privacy.trackingprotection.enabled', true);
 // Devtools
 // ======================
 
+// Enable things by default
 user_pref('devtools.editor.tabsize', 4);
 user_pref('devtools.theme', dark);
 user_pref('devtools.canvasdebugger.enabled', true);
